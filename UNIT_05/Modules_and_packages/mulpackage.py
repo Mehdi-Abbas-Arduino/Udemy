43e04cd633ff46b8b3b8555d139621b98e640397
Mehdi Abbas <askmehdiabbas@gmail.com>
2024-07-29 21:32:14 +0500

    Initial Commit

---
+++ UNIT_05/Modules_and_packages/mulpackage.py
@@ -0,0 +1,8 @@
+def sqrt (a) : 
+    import math 
+    m = math.sqrt(a)
+    print(m)
+if __name__ == "__main__":
+    # It determines that the script is run directly or by importing 
+    # It can organize and seperate the code 
+    sqrt (4)
